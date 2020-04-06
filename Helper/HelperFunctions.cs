107717ef78fa71afc18fdcf92bcf8c7b30a80cd8
Biswajit Nanda <telljeet@gmail.com>
2020-04-06 05:13:16 +0100

    added Helper

---
+++ Helper/HelperFunctions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using BDDFramework_WishList.Helper;
+using Newtonsoft.Json;
+
+namespace specflowstepbystep.Helper
+{
+    public static class HelperFunctions
+    {
+        public static string productname;
+        public static void sleep(int time)
+        {
+            System.Threading.Thread.Sleep(time);
+        }
+
+        public static string get_product_fromjson()
+        {
+            string strResultJson = String.Empty;
+            strResultJson = File.ReadAllText(Config._jsonfilepath);
+            Product str = JsonConvert.DeserializeObject<Product>(strResultJson);
+            return str.Name;
+        }
+    }
+}
---
+++ Helper/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BDDFramework_WishList.Helper
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        
+    }
+}
+
